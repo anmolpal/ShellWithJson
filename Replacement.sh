30ee3842859106de953991b023f0ef52ce1d66b2
CodeMonk <85857368+anmolpal@users.noreply.github.com>
2022-07-25 16:41:35 +0530

    Update Replacement.sh

--- Replacement.sh
+++ Replacement.sh
@@ -16,7 +16,71 @@ sudo touch usar1.txt
 sudo chmod 777 usar1.txt
 echo $usar | sed "s/['\"]//g" > usar1.txt
 usar1=`awk 'FNR ==1 {print $1}' /usr/local/usar1.txt`
-sed -i '/PATH/d' /home/$usar1/.bashrc
+
+
+sudo touch bashline
+sudo chmod 777 bashline
+
+sudo touch abc
+sudo chmod 777 abc
+
+sudo touch txt
+sudo chmod 777 txt
+
+sudo touch tnt
+sudo chmod 777 tnt
+
+sudo touch ab
+sudo chmod 777 ab
+
+cd /usr/local/
+usar=`jq '.user' info.json`
+sudo touch usar1.txt
+sudo chmod 777 usar1.txt
+echo $usar | sed "s/['\"]//g" > usar1.txt
+usar1=`awk 'FNR ==1 {print $1}' /usr/local/usar1.txt`
+#echo $usar1
+
+cd /home/$usar1/
+
+grep 'PATH' /home/$usar1/.bashrc | grep 'hadoop' >> bashline
+while read line
+do
+        IFS=':' #setting space as delimiter
+        read -ra ADDR <<< "$line" #reading str as an array as tokens separated by IFS
+        for i in "${ADDR[@]}"; #accessing each element of array
+        do
+                echo "$i"
+        done >>abc
+        # echo $line
+done < bashline
+
+x="hadoop"
+sed  -i "/\<$x\>/ d"  /home/$usar1/abc
+x="spark"
+sed  -i "/\<$x\>/ d"  /home/$usar1/abc
+uniq abc txt
+#awk '!visited[$0]++' /home/$usar1/txt >> tnt
+
+(readarray -t ARRAY < txt; echo "${ARRAY[*]}") >> ab
+sudo rm -rf abc
+
+while read line
+do
+echo "$line"  >> ~/.bashrc
+done < ab
+cd /usr/local/
+sudo rm -rf tnt
+sudo rm -rf ab
+sudo rm -rf txt
+sudo rm -rf bashline
+sudo rm -rf abc
+cd /home/$usar1/
+sed -i '/\<PATH\>/ { /\<hadoop\>/ d; }' /home/$usar1/.bashrc
+sed -i '/\<PATH\>/ { /\<spark\>/ d; }' /home/$usar1/.bashrc
+
+
+#sed -i '/PATH/d' /home/$usar1/.bashrc
 sed -i '/HADOOP_HOME/d' /home/$usar1/.bashrc
 sed -i '/HADOOP_MAPRED_HOME/d' /home/$usar1/.bashrc
 sed -i '/HADOOP_COMMON_HOME/d' /home/$usar1/.bashrc
@@ -29,7 +93,8 @@ sed -i '/SPARK_HOME/d' /home/$usar1/.bashrc
 sed -i '/PYSPARK_PYTHON/d' /home/$usar1/.bashrc
 sed -i '/PYSPARK_DRIVER_PYTHON/d' /home/$usar1/.bashrc
 sed -i '/HIVE_HOME/d' /home/$usar1/.bashrc
-
+sed -i '/mssql-tools/d' /home/$usar1/.bashrc
+sed -i '/LD_localRARY_PATH/d' /home/$usar1/.bashrc
 cd /usr/local/
 sudo rm -rf usar1.txt
 
@@ -48,7 +113,3 @@ remove=`awk 'FNR ==1 {print $1}' /usr/local/hive.txt`
 #echo $remove
 sudo rm -rf $remove
 sudo rm -rf hive.txt
-
-
-
-
